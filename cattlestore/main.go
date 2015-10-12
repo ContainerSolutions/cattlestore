af34bf8a3d326333965af7310919c8f8a0fb9dc7
Carlos León <mail@carlosleon.info>
2015-10-12 17:42:42 +0200

    Add flag lib for web app.
    We can now read the interface on which the web app will be listening on
    and also the delay that it will take for finishing requests.

--- cattlestore/main.go
+++ cattlestore/main.go
@@ -1,35 +1,49 @@
 package main // import "github.com/ContainerSolutions/cattlestore"
 
 import (
+	"flag"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 	"time"
 )
 
+var (
+	delayRead int
+	delay     time.Duration
+	bind      string
+)
+
+func init() {
+	flag.IntVar(&delayRead, "delay", 10, "the amount of seconds the web server will take to finish serving a request")
+	flag.StringVar(&bind, "bind", ":8080", "ip:port pair where the web server will listen on")
+	delay = time.Duration(delayRead) * time.Second
+}
+
 func main() {
-	health := true
+	flag.Parse()
+
+	healthy := true
 
 	http.HandleFunc("/tick", func(w http.ResponseWriter, r *http.Request) {
 		log.Print("tick")
-		health = false
-		time.Sleep(20 * time.Second)
-		health = true
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		healthy = false
+		time.Sleep(delay)
+		healthy = true
+		fmt.Fprintf(w, "tock")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		var msg string
-		if health == true {
-			msg = "0" // passing
+		if healthy {
+			msg = "yay" // passing
 		} else {
-			msg = "2" // failing
+			msg = "nay" // failing
 			http.Error(w, "oops", 500)
 		}
 		fmt.Fprintf(w, msg)
 	})
 
-	log.Print("Ready...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Ready to listen on %s", bind)
+	log.Fatal(http.ListenAndServe(bind, nil))
 }
