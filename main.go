3c3cbefcc1b1fbd6b3c00e3bb9ccd34f783c50c5
Remmelt Pit <remmelt.pit@container-solutions.com>
2015-11-10 20:30:49 +0100

    This webserver will self destruct after n requests.

--- main.go
+++ main.go
@@ -1,42 +1,64 @@
-package main // import "github.com/ContainerSolutions/cattlestore"
+package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
+const max uint64 = 10
+
 var (
-	delayRead int
+	delayRead uint
 	delay     time.Duration
 	bind      string
+	ops       uint64 = 0
 )
 
+type Message struct {
+	Ops uint64
+	Max uint64
+}
+
 func init() {
-	flag.IntVar(&delayRead, "delay", 3, "the amount of seconds the web server will take to finish serving a request")
+	flag.UintVar(&delayRead, "delay", 1500, "the number of milliseconds the web server will take to finish serving a request")
 	flag.StringVar(&bind, "bind", ":8080", "ip:port pair the web server will listen on")
-	delay = time.Duration(delayRead) * time.Second
+}
+
+func marshal(w http.ResponseWriter, o uint64, m uint64) {
+	if b, err := json.Marshal(Message{o, m}); err != nil {
+		http.Error(w, "Couldn't marshal message", 500)
+	} else {
+		fmt.Fprintf(w, "%s", b)
+	}
 }
 
 func main() {
 	flag.Parse()
-
-	healthy := true
+	delay = time.Duration(delayRead) * time.Millisecond
 
 	http.HandleFunc("/tick", func(w http.ResponseWriter, r *http.Request) {
-		if !healthy {
-			log.Printf("Oh snap!")
-			http.Error(w, "I was busy!", 500)
-			return
+		tmpOps := atomic.LoadUint64(&ops)
+
+		if tmpOps >= max {
+			log.Fatal(fmt.Sprintf("I am so done with you after %d requests", max))
 		}
-		healthy = false
+
+		marshal(w, tmpOps, max)
+
+		log.Print(fmt.Sprintf("tock (%d/%d)", tmpOps+1, max))
+		atomic.AddUint64(&ops, 1)
 		time.Sleep(delay)
-		healthy = true
-		fmt.Fprintf(w, "tock")
 	})
 
-	log.Printf("Ready to listen on %s, delaying each response with %d s", bind, delayRead)
+	http.HandleFunc("/info", func(w http.ResponseWriter, r *http.Request) {
+		marshal(w, atomic.LoadUint64(&ops), max)
+	})
+
+	log.Printf("Ready to serve %d times, delayed %s, on address %s", max, delay, bind)
 	log.Fatal(http.ListenAndServe(bind, nil))
 }
