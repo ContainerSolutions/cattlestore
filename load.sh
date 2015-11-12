c1fce28b522d3a0a38efd6ffee459d2be0dc4067
Remmelt Pit <remmelt.pit@container-solutions.com>
2015-11-12 14:52:17 +0100

    Moving infra stuff to herder

--- load.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-echo Loading cattlestore into Marathon
-
-http POST $(docker-machine ip default):8080/v2/apps @cattlestore.json
-
--- scale.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-echo Scaling cattlestore to $1 instances 
-
-http -b PUT $(docker-machine ip default):8080/v2/apps/cattlestore instances:=$1
-
